7e373f1eb3709c62812ce2f76fc0840b1ef183a2
Colby Cox <wcox@cub.uca.edu>
2024-05-15 19:13:46 -0500

    tabs fixed and text made invisible.

---
+++ index.js
@@ -0,0 +1,7 @@
+// const text=document.getElementById('text')
+
+function showText(input){
+    document.getElementById('text').style.opacity='0';
+    document.getElementById(input).style.opacity='1';
+    // document.getElementById('text').style.display='none';
+}
