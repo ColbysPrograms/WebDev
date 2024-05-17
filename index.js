6540a402801ca70f59fe93009422de12c257f6b4
Colby Cox <wcox@cub.uca.edu>
2024-05-17 15:34:25 -0500

    Text and items now showing up to the side.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-// const text=document.getElementById('text')
 
-function showText(input){
-    document.getElementById('text').style.opacity='0';
-    document.getElementById(input).style.opacity='1';
-    // document.getElementById('text').style.display='none';
+var text=document.getElementById('text');
+var objective=document.getElementById('objective');
+var resume=document.getElementById('resume');
+var projects=document.getElementById('projects');
+var about=document.getElementById('about');
+var contact=document.getElementById('contact');
+objective.addEventListener("click", clickHandler);
+resume.addEventListener('click', clickHandler);
+projects.addEventListener('click', clickHandler);
+about.addEventListener('click', clickHandler);
+contact.addEventListener('click', clickHandler);
+
+function textChange(input){
+    // text.style.visibility="hidden";
+    inputElement=document.getElementById(input);
+    inputElementContent=inputElement.innerHTML;
+    text.innerHTML=inputElementContent;
+    text.style.visibility="visible";
+}
+function textRemove(){
+    text.innerHTML="";
 }
